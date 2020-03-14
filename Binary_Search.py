f90a67f4f355645a74e2ab9871a49366da19a90c
svetockak <svetakeda2504@gmail.com>
2020-03-14 10:15:10 +0700

    added comments

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,33 +1,30 @@
 import random
 
-def Binary_Search(list, number, first_index, last_index):
+
+def Binary_Search(list, number, first_index, last_index):  # Binary Search with recursive
     if first_index > last_index:
         return None
     else:
-        middle_number = (first_index + last_index) // 2
+        middle_number = (first_index + last_index) // 2    # Looking for middle number
         if number == list[middle_number]:
-            return middle_number
-        elif number < list[middle_number]:
+            return middle_number                           # Program stops if middle number = needed number
+        elif number < list[middle_number]:                 # If number smaller or bigger it starts recursive again
             return Binary_Search(list, number, first_index, middle_number - 1)
         else:
             return Binary_Search(list, number, middle_number + 1, last_index)
 
 
-
-def Binary_Search_2(list, number, first_index, last_index):
+def Binary_Search_2(list, number, first_index, last_index):  # Binary Search with iteration
     while first_index + 1 < last_index:
-        middle_number = (first_index + last_index) // 2
+        middle_number = (first_index + last_index) // 2      # Looking for middle number
         if list[middle_number] < number:
-            first_index = middle_number
+            first_index = middle_number                      # First index becomes middle number
         else:
             last_index = middle_number
     return last_index
 
 
-
-
-
-list = []
+list = []                                                    # Making new list
 for i in range(0, 100):
     list.append(random.randint(0, 100))
 list.sort()
@@ -41,5 +38,3 @@ if number > last_index:
 else:
     print(Binary_Search(list, number, first_index, last_index))
     print(Binary_Search_2(list, number, first_index, last_index))
-
-
